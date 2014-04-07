49d0c1ba636b5b9796fdc0d4f35f4297e1536426
Jerome Cheng <jerome@ayulin.net>
2014-04-07 14:13:52 +0800

    Make factory method static.

--- src/net/ayulin/simpleswarmer/Swarm.java
+++ src/net/ayulin/simpleswarmer/Swarm.java
@@ -5,19 +5,19 @@ public class Swarm {
 	int dimensions;
 	EvaluationFunction function;
 	boolean shouldMaximise = true;
-	
+
 	double[] bestPosition;
 	double bestScore;
-	
+
 	double particleResetProbability;
-	
+
 	int numberOfIterations;
 
 	private Swarm() {
-		
+
 	}
-	
-	public Swarm forOptimisationProblem(OptimisationProblem problem) {
+
+	public static Swarm forOptimisationProblem(OptimisationProblem problem) {
 		Swarm s = new Swarm();
 		s.numberOfIterations = 0;
 
@@ -26,18 +26,19 @@ public class Swarm {
 
 		s.particles = new Particle[problem.getNumberOfParticles()];
 		for (int i = 0; i < problem.getNumberOfParticles(); i++) {
-			Particle p = new Particle(problem.getDimensions());
+			Particle p = new Particle(problem.getDimensions(),
+					problem.getFunction());
 			p.setMaxPosition(problem.getMaxPosition());
 			p.setMinPosition(problem.getMinPosition());
 			p.setRandomPosition();
 			p.setRandomVelocity();
-			
-			particles[i] = p;
+
+			s.particles[i] = p;
 		}
-		
+
 		return s;
 	}
-	
+
 	public void setOptimisationStrategy(OptimisationStrategy strategy) {
 		switch (strategy) {
 		case MINIMISE:
@@ -49,19 +50,19 @@ public class Swarm {
 			break;
 		}
 	}
-	
+
 	public void optimise() {
 		numberOfIterations++;
 
 		for (Particle p : particles) {
 			p.iterate(bestPosition);
-			
+
 			double particleBestScore = p.getBestScore();
 			if (particleBestScore > bestScore) {
 				bestScore = particleBestScore;
 				bestPosition = p.getBestPosition();
 			}
-			
+
 			double random = Math.random();
 			if (random < particleResetProbability) {
 				// Reset the particle.
